195e6f4edd1e012a4ffe6eb0850a38613600500d
[LifeBoatOasis] <[lifeboatcare@gmail.com]>
2021-04-01 12:19:06 +0530

    First

---
+++ Assets/Editor/InteractionEditorScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(InteractionHandler))]
+class DecalMeshHelperEditor : Editor {
+  public override void OnInspectorGUI() {
+      DrawDefaultInspector();
+      InteractionHandler handler = (InteractionHandler)target;
+      if(GUILayout.Button("Find Interactables"))
+        handler.interactables = FindObjectsOfType<InteractableScript>();
+  }
+}
---
+++ Assets/InteractableValuesScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableValuesScript : MonoBehaviour
+{
+    public Text logText;
+    private InteractableScript interactableScript;
+    private void Start() {
+        interactableScript = GetComponent<InteractableScript>();
+    }
+    // Start is called before the first frame update
+    void Update()
+    {
+        logText.text = "Total Used by : "+interactableScript.usedBy;
+        foreach(Interaction interaction in interactableScript.interactions)
+            if(interaction.interactableType != InteractableType.Close)
+                logText.text += "\n Interaction Used by : "+interaction.usedBy;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        logText.transform.LookAt(Camera.main.transform.position);
+        logText.transform.position = transform.position + new Vector3(0,1,0);
+    }
+}
